c2ec3c312e9d290b4962e18da0203bc9d4b524a3
IIIlllIIllIIIll <liangbozhan@gmail.com>
2023-10-02 16:08:01 +1300

    Improve nat_routes.sh and bgp_ports.sh

--- bgp_ports.sh
+++ bgp_ports.sh
@@ -3,11 +3,14 @@
 set -e
 set -x
 
-iptables -t nat -A DOCKER -p udp --dport 20000:29999 -j DNAT --to-destination <bgp ipv4 地址>
-iptables -t nat -A POSTROUTING -p udp --dport 20000:29999 --source <bgp ipv4 地址> --destination <bgp ipv4 地址> -j MASQUERADE
-iptables -t filter -I DOCKER-USER -p udp --dport 20000:29999 --destination <bgp ipv4 地址> -j ACCEPT
+V4=<bgp ipv4 地址>
+V6=<bgp ipv6 地址>
 
-ip6tables -t nat -A OUTPUT -p udp --dport 20000:29999 -j DNAT --to-destination <bgp ipv6 地址>
-ip6tables -t nat -A PREROUTING -p udp --dport 20000:29999 -j DNAT --to-destination <bgp ipv6 地址>
-ip6tables -t nat -A POSTROUTING -p udp --dport 20000:29999 --source <bgp ipv6 地址> --destination <bgp ipv6 地址> -j MASQUERADE
-ip6tables -t filter -I FORWARD -p udp --dport 20000:29999 --destination <bgp ipv6 地址> -j ACCEPT
+iptables -t nat -A DOCKER -p udp --dport 20000:29999 -j DNAT --to-destination $V4
+iptables -t nat -A POSTROUTING -p udp --dport 20000:29999 --source $V4 --destination $V4 -j MASQUERADE
+iptables -t filter -I DOCKER-USER -p udp --dport 20000:29999 --destination $V4 -j ACCEPT
+
+ip6tables -t nat -A OUTPUT -p udp --dport 20000:29999 -j DNAT --to-destination $V6
+ip6tables -t nat -A PREROUTING -p udp --dport 20000:29999 -j DNAT --to-destination $V6
+ip6tables -t nat -A POSTROUTING -p udp --dport 20000:29999 --source $V6 --destination $V6 -j MASQUERADE
+ip6tables -t filter -I FORWARD -p udp --dport 20000:29999 --destination $V6 -j ACCEPT
--- nat_routes.sh
+++ nat_routes.sh
@@ -3,5 +3,8 @@
 set -e
 set -x
 
-ip route add 172.20.0.0/14 via <nat 的 IPV4>
-ip route add fd00::/8 via <nat 的 IPV6>
+V4=<nat 的 IPV4>
+V6=<nat 的 IPV6>
+
+ip route add 172.20.0.0/14 via $V4
+ip route add fd00::/8 via $V6
